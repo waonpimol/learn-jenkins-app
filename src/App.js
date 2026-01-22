78f0fa97960706e99e351651771a14f50b5b3531
woaonpimol <aonpimolw66@nu.ac.th>
2026-01-22 08:59:13 +0700

    add .

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy in DevOps.
+          Learn Jenkins on Udemy in DevOpss
         </a>
       </header>
       <p>
